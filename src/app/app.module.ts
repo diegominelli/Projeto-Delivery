19fb14e9321dc28d17bdb31c6ca28254fd5e2e9e
diegominelli <diego.minelli@hotmail.com>
2020-12-30 20:11:23 -0300

    Criacao de pastas e componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CategoriesComponent } from './components/categories/categories.component';
+import { RestaurantsListComponent } from './components/restaurants-list/restaurants-list.component';
+import { FiltersComponent } from './components/filters/filters.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { AccountComponent } from './components/account/account.component';
+import { RestaurantCardComponent } from './components/restaurant-card/restaurant-card.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CategoriesComponent,
+    RestaurantsListComponent,
+    FiltersComponent,
+    SearchBarComponent,
+    AccountComponent,
+    RestaurantCardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/core/models/user/user.ts
@@ -0,0 +1,7 @@
+import { mainModule } from "process";
+
+export class User {
+  _id: string;
+  name: string;
+  email: string;
+}
