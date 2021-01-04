90ca1de52b38dade530504403df20b0815748067
diegominelli <diego.minelli@hotmail.com>
2021-01-04 20:41:18 -0300

    Inserindo html, scss e ts do login

--- src/app/components/restaurant-card/restaurant-card.component.ts
+++ src/app/components/restaurant-card/restaurant-card.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-restaurant-card',
   templateUrl: './restaurant-card.component.html',
-  styleUrls: ['./restaurant-card.component.sass']
+  styleUrls: ['./restaurant-card.component.scss']
 })
 export class RestaurantCardComponent implements OnInit {
 
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.sass']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+  hide = true;
+  constructor(
+    private router: Router,
+    private snackBar: MatSnackBar ) { }
 
-  ngOnInit(): void {
+
+  ngOnInit () {
+    
+    this.loginForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  login(): void {
+   
+  }
+
+  addUser() {
+    this.router.navigate(['/usuarios/novo']);
+  }
+
+  openSnackBar(message: string, config) {
+    this.snackBar.open(message, 'fechar', {
+      duration: 9000,
+      panelClass: ['style-error']
+    });
   }
 
 }
